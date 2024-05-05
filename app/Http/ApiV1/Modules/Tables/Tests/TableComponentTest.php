da8db4e3aa2bf1b31abbacf60bd4536ee16911f1
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2024-05-05 22:58:21 +0300

    1st try to fix docs

--- app/Http/ApiV1/Modules/Tables/Tests/TableComponentTest.php
+++ app/Http/ApiV1/Modules/Tables/Tests/TableComponentTest.php
@@ -10,52 +10,52 @@ use function Pest\Laravel\putJson;
 uses(ApiV1ComponentTestCase::class);
 uses()->group('component');
 
-test('GET http://localhost:8000/tables 200', function () {
-    getJson('http://localhost:8000/tables')
+test('GET http://localhost:8000/api/v1/tables 200', function () {
+    getJson('http://localhost:8000/api/v1/tables')
         ->assertStatus(200);
 });
 
-test('GET http://localhost:8000/tables 404', function () {
-    getJson('http://localhost:8000/tables')
+test('GET http://localhost:8000/api/v1/tables 404', function () {
+    getJson('http://localhost:8000/api/v1/tables')
         ->assertStatus(404);
 });
 
-test('POST http://localhost:8000/tables 200', function () {
-    postJson('http://localhost:8000/tables')
+test('POST http://localhost:8000/api/v1/tables 200', function () {
+    postJson('http://localhost:8000/api/v1/tables')
         ->assertStatus(200);
 });
 
-test('POST http://localhost:8000/tables 404', function () {
-    postJson('http://localhost:8000/tables')
+test('POST http://localhost:8000/api/v1/tables 404', function () {
+    postJson('http://localhost:8000/api/v1/tables')
         ->assertStatus(404);
 });
 
-test('GET http://localhost:8000/tables/{id} 200', function () {
-    getJson('http://localhost:8000/tables/{id}')
+test('GET http://localhost:8000/api/v1/tables/{id} 200', function () {
+    getJson('http://localhost:8000/api/v1/tables/1')
         ->assertStatus(200);
 });
 
-test('GET http://localhost:8000/tables/{id} 404', function () {
-    getJson('http://localhost:8000/tables/{id}')
+test('GET http://localhost:8000/api/v1/tables/{id} 404', function () {
+    getJson('http://localhost:8000/api/v1/tables/1000')
         ->assertStatus(404);
 });
 
-test('PUT http://localhost:8000/tables/{id} 200', function () {
-    putJson('http://localhost:8000/tables/{id}')
+test('PUT http://localhost:8000/api/v1/tables/{id} 200', function () {
+    putJson('http://localhost:8000/api/v1/tables/1')
         ->assertStatus(200);
 });
 
-test('PUT http://localhost:8000/tables/{id} 404', function () {
-    putJson('http://localhost:8000/tables/{id}')
+test('PUT http://localhost:8000/api/v1/tables/{id} 404', function () {
+    putJson('http://localhost:8000/api/v1/tables/1000')
         ->assertStatus(404);
 });
 
-test('DELETE http://localhost:8000/tables/{id} 204', function () {
-    delete('http://localhost:8000/tables/{id}')
+test('DELETE http://localhost:8000/api/v1/tables/{id} 204', function () {
+    delete('http://localhost:8000/api/v1/tables/1')
         ->assertStatus(204);
 });
 
-test('DELETE http://localhost:8000/tables/{id} 404', function () {
-    delete('http://localhost:8000/tables/{id}')
+test('DELETE http://localhost:8000/api/v1/tables/{id} 404', function () {
+    delete('http://localhost:8000/api/v1/tables/1000')
         ->assertStatus(404);
 });
