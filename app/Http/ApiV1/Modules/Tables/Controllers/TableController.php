243f2da89218415c72d4a751911c0f2e29be4a49
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2024-05-07 09:23:18 +0300

    Исправлена спецификация API, написаны тесты

--- app/Http/ApiV1/Modules/Tables/Controllers/TableController.php
+++ app/Http/ApiV1/Modules/Tables/Controllers/TableController.php
@@ -7,22 +7,43 @@ use App\Http\ApiV1\Modules\Tables\Requests\AddTableRequest;
 use App\Http\ApiV1\Modules\Tables\Requests\DeleteTableRequest;
 use App\Http\ApiV1\Modules\Tables\Requests\EditTableRequest;
 use App\Http\ApiV1\Modules\Tables\Resources\TableResource;
+use App\Http\ApiV1\Modules\Tables\Resources\TablesResource;
+use App\Http\ApiV1\Support\Resources\EmptyResource;
 
 class TableController
 {
-    public function getTables(): array
+    private function checkHeaders(): void
+    {
+        $data = env('X_API_SECRET_DATA');
+        $algo = env('X_API_SECRET_ALGORITHM');
+        $key = env('X_API_SECRET_KEY');
+        $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+        $value = openssl_encrypt($data, $algo, $key, 0, $iv);           //HDHffpH3pqY64svULpEFhg==
+        $headers = request()->header();
+        if (!array_key_exists('x-api-secret', $headers) || $headers['x-api-secret'][0] != $value) {
+            abort(403);
+        }
+    }
+
+    public function getTables(): TablesResource
     {
         //Возвращает список столиков
-        $tables = Table::all();
-        return TableResource::collection($tables)->toArray(request());
+        $this->checkHeaders();
+        return new TablesResource(Table::query()->get());
     }
 
     public function addTable(AddTableRequest $request): TableResource
     {
         //Добавляет столик
+        $this->checkHeaders();
         $table = new Table();
-        $table->seats = $request->get('seats');
-        $table->location = $request->get('location');
+        $seats = $request->get('seats');
+        $location = $request->get('location');
+        if ($seats < 1) {
+            abort(400);
+        }
+        $table->seats = $seats;
+        $table->location = $location;
         $table->save();
 
         return new TableResource($table);
@@ -31,26 +52,34 @@ class TableController
     public function getTable(int $id): TableResource
     {
         //Возвращает столик
+        $this->checkHeaders();
         return new TableResource(Table::query()->findOrFail($id));
     }
 
     public function editTable(int $id, EditTableRequest $request): TableResource
     {
         //Изменяет столик
+        $this->checkHeaders();
         $table = Table::query()->findOrFail($id);
-        $table->seats = $request->get('seats');
-        $table->location = $request->get('location');
+        $seats = $request->get('seats');
+        $location = $request->get('location');
+        if ($seats < 1) {
+            abort(400);
+        }
+        $table->seats = $seats;
+        $table->location = $location;
         $table->save();
 
         return new TableResource($table);
     }
 
-    public function deleteTable(int $id, DeleteTableRequest $request): TableResource
+    public function deleteTable(int $id, DeleteTableRequest $request): EmptyResource
     {
         //Удаляет столик
+        $this->checkHeaders();
         $table = Table::query()->findOrFail($id);
         $table->delete();
 
-        return new TableResource($table);
+        return new EmptyResource();
     }
 }
--- app/Http/ApiV1/Modules/Tables/Tests/TableComponentTest.php
+++ app/Http/ApiV1/Modules/Tables/Tests/TableComponentTest.php
@@ -11,51 +11,126 @@ uses(ApiV1ComponentTestCase::class);
 uses()->group('component');
 
 test('GET http://localhost:8000/api/v1/tables 200', function () {
-    getJson('http://localhost:8000/api/v1/tables')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    getJson('http://localhost:8000/api/v1/tables', ['X-API-SECRET' => $value])
         ->assertStatus(200);
 });
 
-test('GET http://localhost:8000/api/v1/tables 404', function () {
-    getJson('http://localhost:8000/api/v1/tables')
-        ->assertStatus(404);
+test('GET http://localhost:8000/api/v1/tables 403', function () {
+    getJson('http://localhost:8000/api/v1/tables', ['X-API-SECRET' => 'wrong'])
+        ->assertStatus(403);
 });
 
-test('POST http://localhost:8000/api/v1/tables 200', function () {
-    postJson('http://localhost:8000/api/v1/tables')
-        ->assertStatus(200);
+test('POST http://localhost:8000/api/v1/tables 201', function () {
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    postJson('http://localhost:8000/api/v1/tables', ['seats' => 10, 'location' => 'test'], ['X-API-SECRET' => $value])
+        ->assertStatus(201);
 });
 
-test('POST http://localhost:8000/api/v1/tables 404', function () {
-    postJson('http://localhost:8000/api/v1/tables')
-        ->assertStatus(404);
+test('POST http://localhost:8000/api/v1/tables 400', function () {
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    postJson('http://localhost:8000/api/v1/tables', ['seats' => -1, 'location' => 'test'], ['X-API-SECRET' => $value])
+        ->assertStatus(400);
+});
+
+test('POST http://localhost:8000/api/v1/tables 403', function () {
+    postJson('http://localhost:8000/api/v1/tables', ['seats' => 10, 'location' => 'test'], ['X-API-SECRET' => 'wrong'])
+        ->assertStatus(403);
 });
 
 test('GET http://localhost:8000/api/v1/tables/{id} 200', function () {
-    getJson('http://localhost:8000/api/v1/tables/1')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    getJson('http://localhost:8000/api/v1/tables/1', ['X-API-SECRET' => $value])
         ->assertStatus(200);
 });
 
+test('GET http://localhost:8000/api/v1/tables/{id} 403', function () {
+    getJson('http://localhost:8000/api/v1/tables/1', ['X-API-SECRET' => 'wrong'])
+        ->assertStatus(403);
+});
+
 test('GET http://localhost:8000/api/v1/tables/{id} 404', function () {
-    getJson('http://localhost:8000/api/v1/tables/1000')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    getJson('http://localhost:8000/api/v1/tables/1000', ['X-API-SECRET' => $value])
         ->assertStatus(404);
 });
 
 test('PUT http://localhost:8000/api/v1/tables/{id} 200', function () {
-    putJson('http://localhost:8000/api/v1/tables/1')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    putJson('http://localhost:8000/api/v1/tables/1', ['seats' => 10, 'location' => 'test'], ['X-API-SECRET' => $value])
         ->assertStatus(200);
 });
 
+test('PUT http://localhost:8000/api/v1/tables/{id} 400', function () {
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    putJson('http://localhost:8000/api/v1/tables/1', ['seats' => -1, 'location' => 'test'], ['X-API-SECRET' => $value])
+        ->assertStatus(400);
+});
+
+test('PUT http://localhost:8000/api/v1/tables/{id} 403', function () {
+    putJson('http://localhost:8000/api/v1/tables/1', ['seats' => 10, 'location' => 'test'], ['X-API-SECRET' => 'wrong'])
+        ->assertStatus(403);
+});
+
 test('PUT http://localhost:8000/api/v1/tables/{id} 404', function () {
-    putJson('http://localhost:8000/api/v1/tables/1000')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    putJson('http://localhost:8000/api/v1/tables/1000', ['seats' => 10, 'location' => 'test'], ['X-API-SECRET' => $value])
         ->assertStatus(404);
 });
 
-test('DELETE http://localhost:8000/api/v1/tables/{id} 204', function () {
-    delete('http://localhost:8000/api/v1/tables/1')
-        ->assertStatus(204);
+test('DELETE http://localhost:8000/api/v1/tables/{id} 200', function () {
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    delete('http://localhost:8000/api/v1/tables/1', [], ['X-API-SECRET' => $value])
+        ->assertStatus(200);
+});
+
+test('DELETE http://localhost:8000/api/v1/tables/{id} 403', function () {
+    delete('http://localhost:8000/api/v1/tables/1', [], ['X-API-SECRET' => 'wrong'])
+        ->assertStatus(403);
 });
 
 test('DELETE http://localhost:8000/api/v1/tables/{id} 404', function () {
-    delete('http://localhost:8000/api/v1/tables/1000')
+    $data = env('X_API_SECRET_DATA');
+    $algo = env('X_API_SECRET_ALGORITHM');
+    $key = env('X_API_SECRET_KEY');
+    $iv = str_repeat('0', openssl_cipher_iv_length($algo));
+    $value = openssl_encrypt($data, $algo, $key, 0, $iv);
+    delete('http://localhost:8000/api/v1/tables/1000', [], ['X-API-SECRET' => $value])
         ->assertStatus(404);
 });
